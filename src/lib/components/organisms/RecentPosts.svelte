872240cb02ad28fd62f2599dd60364a5e272889a
Geek Fleek <radrex460@gmail.com>
2024-08-01 07:06:59 -0700

    UI tweaks

--- src/lib/components/organisms/RecentPosts.svelte
+++ src/lib/components/organisms/RecentPosts.svelte
@@ -10,7 +10,7 @@
 <ContentSection
 	id="recent-posts"
 	title="Blog posts"
-	description="This section shows the 4 most recent blog posts. Check them out for tips on how to get started!"
+	description="Check out our blogs on cleaning hacks and other educative content on cleanliness"
 	align="left"
 >
 	<div slot="button">
